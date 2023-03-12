3488ef41b26db2f0cab0a77fa052570d37143cdd
Andy Nguyen <andynguyen0114@gmail.com>
2023-03-11 18:11:13 -0600

    New JS validation

---
+++ ValidateForm.js
@@ -0,0 +1,8 @@
+// input variables to validate on form
+const usernameE1 = document.querySelector('#username');
+const emailE1 = document.querySelector('#email');
+const passwordE1 = document.querySelector("#password");
+const confirmPasswordE1 = document.querySelector("#confirm-password");
+
+const form = document.querySelector("#signup");
+
